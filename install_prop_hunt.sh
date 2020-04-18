a8db6ea79157b583ce93df567e445c90e5d2e7a4
Adam Sheppard <t7.shab@gmail.com>
2020-04-18 17:37:52 +0100

    Corrected file pathing

--- install_prop_hunt.sh
+++ install_prop_hunt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh   
-
-cd /server_1/garrysmod/gamemodes
+cd ~
+cd server_1/garrysmod/gamemodes
 wget https://github.com/andrewtheis/prophunt/archive/master.zip
 unzip prophunt-master.zip
 mv prophunt-master prophunt
