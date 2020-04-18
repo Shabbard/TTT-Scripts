7ad35d114ae14805669de0fdbab8da062d29c155
Adam Sheppard <t7.shab@gmail.com>
2020-04-18 17:40:47 +0100

    Corrected zipname and remove after unzip

--- install_prop_hunt.sh
+++ install_prop_hunt.sh
@@ -5,8 +5,9 @@ sudo apt install unzip -y
 cd ~
 cd server_1/garrysmod/gamemodes
 wget https://github.com/andrewtheis/prophunt/archive/master.zip
-unzip prophunt-master.zip
+unzip master.zip
 mv prophunt-master prophunt
+rm master.zip
 cd ~
 cd TTT-Scripts
 ./run_prop_hunt.sh
